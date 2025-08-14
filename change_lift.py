337bed6fef0a6121c652a023f0cb6551fa23a000
cwl <2351126253@qq.com>
2025-08-14 16:09:29 +0800

    修改

---
+++ change_lift.py
@@ -0,0 +1,18 @@
+from Robot.sensor.lift import SerialLiftingMotor
+
+#可能要改以下几个地方的升降机高度
+# 初始化
+# 识别处方单
+# 抓取药品逻辑 三层 四层
+# 放置药框
+
+app = SerialLiftingMotor()
+app.cmd_vel_callback(100)
+
+
+
+
+
+
+
+
---
+++ get_state.py
@@ -0,0 +1,19 @@
+from Robotic_Arm.rm_robot_interface import *
+
+
+left_robot = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
+left_handle = left_robot.rm_create_robot_arm("192.168.0.19", 8080)
+
+state = left_robot.rm_get_current_arm_state(left_handle)
+print(state)
+
+right_robot = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
+right_handle = right_robot.rm_create_robot_arm("192.168.0.18", 8080)
+state = right_robot.rm_get_current_arm_state(right_handle)
+print(state)
+
+
+
+
+
+
